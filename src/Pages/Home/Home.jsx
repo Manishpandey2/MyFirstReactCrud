8644d8a8d7d2037133370ecfcb46d74406d49793
Manishpande2 <biraalogang@gmail.com>
2025-06-17 17:52:46 +0545

    applying few css to grid like structure in screen

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import "./Home.css";
 import axios from "axios";
 
 const Home = () => {
-  const [count, setcount] = useState([]);
+  const [products, setProducts] = useState([]);
   const fetchproduct = async () => {
     const response = await axios.get(
       "https://678a2ed7dd587da7ac291234.mockapi.io/products"
     );
 
-    setcount(response.data);
+    setProducts(response.data);
   };
 
   useEffect(() => {
@@ -20,18 +20,20 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      {count.map((product, index) => {
-        const validImage = `https://picsum.photos/seed/${index}/300/200`;
-        return (
-          <div key={product.id} className="card">
-            <img src={validImage} alt="Product Image" />
-            <div className="card-content">
-              <h2>{product.ProductName}</h2>
-              <p>{product.ProductDescription}</p>
+      <div className="productContainer">
+        {products.map((product, index) => {
+          const validImage = `https://picsum.photos/seed/${index}/300/200`;
+          return (
+            <div key={product.id} className="card">
+              <img src={validImage} alt="Product Image" />
+              <div className="card-content">
+                <h2>{product.ProductName}</h2>
+                <p>{product.ProductDescription}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
