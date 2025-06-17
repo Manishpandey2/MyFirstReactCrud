8e4528c43d80294b9314b51ff96ff38c5e73f7a9
Manishpande2 <biraalogang@gmail.com>
2025-06-17 16:59:50 +0545

    image not rendering problem fixed( by changing image source)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,41 +1,39 @@
-
-import { useEffect, useState } from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import './Home.css'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
-    const [count,setcount] =useState([])
-    const fetchproduct = async()=>{
-        const response = await axios.get("https://678a2ed7dd587da7ac291234.mockapi.io/products")
-        setcount(response.data)
-    }
+  const [count, setcount] = useState([]);
+  const fetchproduct = async () => {
+    const response = await axios.get(
+      "https://678a2ed7dd587da7ac291234.mockapi.io/products"
+    );
 
-    useEffect(()=>{
-        fetchproduct()
-    },[])
-    
-    
+    setcount(response.data);
+  };
+
+  useEffect(() => {
+    fetchproduct();
+  }, []);
+
   return (
     <>
-    <Navbar />
-        {
-            count.map((product)=>{
-                return(
-                    <div key={product.id} className="card">
-        <img src={product.ProductImage} alt="Product Image" />
-        <div className="card-content">
-            <h2>{product.ProductName}</h2>
-            <p>{product.ProductDescription}</p>
-        </div>
-    </div>
-                )
-                
-            })
-        
-        }
+      <Navbar />
+      {count.map((product, index) => {
+        const validImage = `https://picsum.photos/seed/${index}/300/200`;
+        return (
+          <div key={product.id} className="card">
+            <img src={validImage} alt="Product Image" />
+            <div className="card-content">
+              <h2>{product.ProductName}</h2>
+              <p>{product.ProductDescription}</p>
+            </div>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
